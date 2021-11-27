6fa3e454aa8b1680681d2481786242b0b9bbf136
Christoph Reiter <reiter.christoph@gmail.com>
2021-11-27 10:32:47 +0100

    Enable redis persistence again
    In case redis gets updated you need to restart mirrorbits to get the
    mirros back, which is confusing. So persist it, but clear the DB on
    mirrorbits start.

--- mirrorbits/init.sh
+++ mirrorbits/init.sh
@@ -2,14 +2,15 @@
 
 set -e
 
+# Throw out the redis db, mirrorbits gets confused easily when adding/removing mirrors,
+# so always start fresh
+redis-cli -h redis FLUSHALL
+
 while [ ! -f /usr/share/GeoIP/GeoLite2-ASN.mmdb ] || [ ! -f /usr/share/GeoIP/GeoLite2-ASN.mmdb ]
 do
     echo "waiting for GeoIP data to be loaded"
-    sleep 2
+    sleep 1
 done
-
-# Throw out the redis db, mirrorbits gets confused easily when adding/removing mirrors
-redis-cli -h redis FLUSHALL
 
 mirrorbits daemon --debug &
 pid=$!
@@ -17,11 +18,8 @@ pid=$!
 while ! mirrorbits list
 do
   echo "waiting for server"
-  sleep 3
+  sleep 1
 done
-
-# remove all mirrors
-mirrorbits list -state=false | tail -n +2 | xargs --no-run-if-empty -n1 mirrorbits remove -f
 
 # add all mirrors
 if [ -f "add_mirrors.sh" ]; then
