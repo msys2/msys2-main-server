670c5e6182554dc0ec7c7781f819e992b7b5d226
Christoph Reiter <reiter.christoph@gmail.com>
2022-02-24 09:15:49 +0100

    tests: adjust test for new redirect and add some more tests

--- test.py
+++ test.py
@@ -10,6 +10,9 @@ class Test(unittest.TestCase):
 
     TIMEOUT = 10
 
+    def assertHSTS(self, response):
+        self.assertTrue(response.headers.get("Strict-Transport-Security"))
+
     def test_rsync(self):
         out = subprocess.check_output(
             ["rsync", "--list-only", "rsync://repo.msys2.org/builds/"],
@@ -22,6 +25,7 @@ class Test(unittest.TestCase):
     def test_repo(self):
         with urlopen("https://repo.msys2.org", timeout=self.TIMEOUT) as r:
             self.assertEqual(r.url, "https://repo.msys2.org")
+            self.assertHSTS(r)
             out = r.read().decode()
             self.assertTrue("distrib" in out)
             self.assertTrue("mingw" in out)
@@ -29,29 +33,44 @@ class Test(unittest.TestCase):
 
     def test_repo_redirects(self):
         with urlopen("https://repo.msys2.org/mingw/sources", timeout=self.TIMEOUT) as r:
+            self.assertHSTS(r)
             self.assertEqual(r.url, "https://repo.msys2.org/mingw/sources/")
         with urlopen("http://repo.msys2.org/mingw/sources", timeout=self.TIMEOUT) as r:
-            self.assertEqual(r.url, "http://repo.msys2.org/mingw/sources/")
+            self.assertHSTS(r)
+            self.assertEqual(r.url, "https://repo.msys2.org/mingw/sources/")
 
     def test_stagingrepo(self):
         with urlopen("https://repo.msys2.org/staging/", timeout=self.TIMEOUT) as r:
             self.assertEqual(r.url, "https://repo.msys2.org/staging/")
+            self.assertHSTS(r)
             out = r.read().decode()
             self.assertTrue("staging.db" in out)
         with urlopen("https://repo.msys2.org/staging", timeout=self.TIMEOUT) as r:
             self.assertEqual(r.url, "https://repo.msys2.org/staging/")
+            self.assertHSTS(r)
             out = r.read().decode()
             self.assertTrue("staging.db" in out)
+        with urlopen("http://repo.msys2.org/staging", timeout=self.TIMEOUT) as r:
+            self.assertEqual(r.url, "https://repo.msys2.org/staging/")
+            self.assertHSTS(r)
+        with urlopen("http://repo.msys2.org/staging/", timeout=self.TIMEOUT) as r:
+            self.assertEqual(r.url, "https://repo.msys2.org/staging/")
+            self.assertHSTS(r)
 
     def test_stats(self):
         with urlopen("https://repo.msys2.org/stats/", timeout=self.TIMEOUT) as r:
             self.assertEqual(r.url, "https://repo.msys2.org/stats/")
+            self.assertHSTS(r)
             out = r.read().decode()
             self.assertTrue("Network Traffic" in out)
         with urlopen("https://repo.msys2.org/stats", timeout=self.TIMEOUT) as r:
             self.assertEqual(r.url, "https://repo.msys2.org/stats/")
+            self.assertHSTS(r)
             out = r.read().decode()
             self.assertTrue("Network Traffic" in out)
+        with urlopen("http://repo.msys2.org/stats/", timeout=self.TIMEOUT) as r:
+            self.assertEqual(r.url, "https://repo.msys2.org/stats/")
+            self.assertHSTS(r)
 
     def test_redirects(self):
         schemes = ["http", "https"]
@@ -72,17 +91,31 @@ class Test(unittest.TestCase):
     def test_packages(self):
         with urlopen("http://packages.msys2.org", timeout=self.TIMEOUT) as r:
             self.assertTrue(r.url.startswith("https://packages.msys2.org"))
+            self.assertHSTS(r)
             out = r.read().decode()
             self.assertTrue("<html" in out)
         with urlopen("https://packages.msys2.org", timeout=self.TIMEOUT) as r:
             self.assertTrue(r.url.startswith("https://packages.msys2.org"))
+            self.assertHSTS(r)
             out = r.read().decode()
             self.assertTrue("<html" in out)
         with urlopen("https://packages.msys2.org/api", timeout=self.TIMEOUT) as r:
             self.assertTrue(r.url.startswith("https://packages.msys2.org/api"))
+            self.assertHSTS(r)
             out = r.read().decode()
             self.assertTrue("<html" in out)
+        with urlopen("http://packages.msys2.org", timeout=self.TIMEOUT) as r:
+            self.assertHSTS(r)
+            self.assertTrue(r.url.startswith("https://packages.msys2.org"))
 
+    def test_mirror(self):
+        with urlopen("http://mirror.msys2.org", timeout=self.TIMEOUT) as r:
+            self.assertTrue(r.url.startswith("https://repo.msys2.org"))
+            self.assertHSTS(r)
+            out = r.read().decode()
+            self.assertTrue("distrib" in out)
+            self.assertTrue("mingw" in out)
+            self.assertTrue("msys" in out)
 
 if __name__ == '__main__':
     unittest.main()
