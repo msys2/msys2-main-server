2296b0059d7d90854394c7e7495b429b196c4136
Christoph Reiter <reiter.christoph@gmail.com>
2023-02-26 12:02:13 +0100

    Port the mirror tests to Python and run them in CI

--- tests/test.py
+++ tests/test.py
@@ -1,14 +1,52 @@
 #!/usr/bin/env python3
 
+import os
+import re
 import unittest
 import subprocess
 import itertools
+import ssl
 from urllib.request import urlopen
+
+
+def get_mirrors():
+    """Returns a list of URLs for all our mirrors"""
+
+    DIR = os.path.dirname(os.path.realpath(__file__))
+    mirrors = set()
+    script = os.path.join(DIR, "..", "services", "mirrorbits", "add_mirrors.sh")
+    with open(script, "r", encoding="utf-8") as h:
+        for line in h.readlines():
+            if line.startswith("#"):
+                continue
+            mirrors.update(
+                re.findall('((?:http|https)://[a-zA-Z0-9./?=_%:-]*)', line))
+    return sorted(mirrors)
+
+
+def get_timeout():
+    timeout = 25
+    if "CI" in os.environ:
+        timeout *= 4
+    return timeout
 
 
 class Test(unittest.TestCase):
 
-    TIMEOUT = 10
+    TIMEOUT = get_timeout()
+
+    def test_mirrors_support_tls12(self):
+        # https://github.com/msys2/msys2.github.io/issues/204
+        context = ssl.create_default_context()
+        context.minimum_version = ssl.TLSVersion.TLSv1_2
+        context.maximum_version = ssl.TLSVersion.TLSv1_2
+        for url in get_mirrors():
+            somefile = url.rstrip("/") + "/lastsync"
+            try:
+                with urlopen(somefile, context=context, timeout=self.TIMEOUT):
+                    pass
+            except Exception as exc:
+                raise Exception(somefile) from exc
 
     def assertHSTS(self, response):
         self.assertTrue(response.headers.get("Strict-Transport-Security"))
