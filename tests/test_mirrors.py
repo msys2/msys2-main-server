9e7badbdcd11dd5e2cc3cb937f3fb7a8a17731e6
Christoph Reiter <reiter.christoph@gmail.com>
2023-04-19 17:33:53 +0200

    tests: add a basic download speed test
    just to see if something is very, very slow

--- tests/test_mirrors.py
+++ tests/test_mirrors.py
@@ -4,6 +4,7 @@ import os
 import re
 import unittest
 import ssl
+import time
 from urllib.request import urlopen
 
 
@@ -39,6 +40,28 @@ class TestMirrors(unittest.TestCase):
             except Exception as exc:
                 raise Exception(somefile) from exc
 
+    def test_download_speed(self):
+        # https://github.com/msys2/msys2.github.io/issues/276
+        for url in get_mirrors():
+            somelargefile = url.rstrip("/") + "/distrib/msys2-x86_64-latest.tar.xz"
+
+            chunk_size = 1024 * 50
+            chunks = 50
+            read_timeout = chunk_size * chunks / 100000
+            try:
+                with urlopen(somelargefile, timeout=self.TIMEOUT) as f:
+                    rt = time.time()
+                    for i in range(chunks):
+                        chunk = f.read(chunk_size)
+                        if not chunk:
+                            break
+                        if time.time() - rt > read_timeout:
+                            raise Exception("Read timeout")
+            except Exception as exc:
+                raise Exception(somelargefile) from exc
+            else:
+                duration = time.time() - rt
+                print("Downloading:", chunk_size * chunks / (1024 ** 2), "MB, in", duration, ",timeout:", read_timeout, somelargefile)
 
 if __name__ == '__main__':
     unittest.main()
