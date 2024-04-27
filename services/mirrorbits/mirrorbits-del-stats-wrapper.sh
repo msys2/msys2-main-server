4f94cd21763118779344da99d7e31b0e09f0c0b2
Christoph Reiter <reiter.christoph@gmail.com>
2024-04-27 09:08:42 +0200

    mirrorbits: fix passing the host to mirrorbits-del-stats
    It has no logic for separating forwarded and its own args, and it
    just stops at the first unknown option, so pass "-p" first so "-h"
    isn't interpreted as "--help"
    
    See https://github.com/etix/mirrorbits/pull/177

--- services/mirrorbits/mirrorbits-del-stats-wrapper.sh
+++ services/mirrorbits/mirrorbits-del-stats-wrapper.sh
@@ -3,4 +3,4 @@
 REDIS_HOST=$(grep "RedisAddress" /etc/mirrorbits.conf | cut -d ':' -f 2 | xargs)
 REDIS_PORT=$(grep "RedisAddress" /etc/mirrorbits.conf | cut -d ':' -f 3 | xargs)
 
-/usr/bin/contrib/mirrorbits-del-stats "$@" -h "$REDIS_HOST" -p "$REDIS_PORT"
+/usr/bin/contrib/mirrorbits-del-stats "$@" -p "$REDIS_PORT" -h "$REDIS_HOST"
