d06908a05ee0af5a32d2747154d8ac2b0d270467
Christoph Reiter <reiter.christoph@gmail.com>
2024-05-09 11:27:58 +0200

    install_to_host: run ldconfig at the end
    so libalpm is found

--- services/staging/install_to_host.sh
+++ services/staging/install_to_host.sh
@@ -4,3 +4,4 @@ set -e
 
 docker build --tag ubuntu-pacman --target installed .
 docker run --rm ubuntu-pacman tar -czf - /etc/{makepkg,pacman}.conf /var/cache/pacman/pkg /var/lib/pacman/ /usr/local/{bin,include,lib,share} | tar -xzf - -C /
+ldconfig
